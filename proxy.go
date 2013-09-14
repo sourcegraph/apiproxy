e71430bfc3574026e994983b0d4a910fadb6f5b4
Quinn Slack <qslack@qslack.com>
2013-09-13 21:46:29 -0700

    NewSingleHostReverseProxy, which wraps net/http/httputil.NewSingleHostReverseProxy and sets the Host header based on the URL's host.

--- proxy.go
+++ proxy.go
@@ -2,12 +2,25 @@ package apiproxy
 
 import (
 	"github.com/gregjones/httpcache"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
 
 func NewCachingSingleHostReverseProxy(target *url.URL) *httputil.ReverseProxy {
-	proxy := httputil.NewSingleHostReverseProxy(target)
+	proxy := NewSingleHostReverseProxy(target)
 	proxy.Transport = httpcache.NewMemoryCacheTransport()
+	return proxy
+}
+
+// NewSingleHostReverseProxy wraps net/http/httputil.NewSingleHostReverseProxy
+// and sets the Host header.
+func NewSingleHostReverseProxy(url *url.URL) *httputil.ReverseProxy {
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	oldDirector := proxy.Director
+	proxy.Director = func(r *http.Request) {
+		oldDirector(r)
+		r.Host = url.Host
+	}
 	return proxy
 }
