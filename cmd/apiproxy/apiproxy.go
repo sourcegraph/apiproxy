912efbff9a405b96fcc5904bde87b7dafe528a8c
Quinn Slack <qslack@qslack.com>
2013-09-14 18:05:25 -0700

    apiproxy -only-revalidate-older-than

--- cmd/apiproxy/apiproxy.go
+++ cmd/apiproxy/apiproxy.go
@@ -14,6 +14,7 @@ import (
 
 var bindAddr = flag.String("http", ":8080", "HTTP bind address for proxy")
 var neverRevalidate = flag.Bool("never-revalidate", false, "never revalidate cached responses (use them regardless of age)")
+var onlyRevalOlderThanStr = flag.String("only-revalidate-older-than", "", "only revalidate cached responses older than this duration (extends cache duration)")
 
 func main() {
 	flag.Usage = func() {
@@ -30,6 +31,8 @@ func main() {
 		fmt.Fprintf(os.Stderr, "\t    $ apiproxy http://example.com\n\n")
 		fmt.Fprintf(os.Stderr, "\t... and never revalidate cached responses:\n")
 		fmt.Fprintf(os.Stderr, "\t    $ apiproxy -never-revalidate http://example.com\n\n")
+		fmt.Fprintf(os.Stderr, "\t... and only revalidate cached responses older than an hour:\n")
+		fmt.Fprintf(os.Stderr, "\t    $ apiproxy -only-revalidate-older-than=1h http://example.com\n\n")
 		fmt.Fprintln(os.Stderr)
 		os.Exit(1)
 	}
@@ -44,17 +47,32 @@ func main() {
 		os.Exit(1)
 	}
 
+	var onlyRevalOlderThan time.Duration
+	if *onlyRevalOlderThanStr != "" {
+		onlyRevalOlderThan, err = time.ParseDuration(*onlyRevalOlderThanStr)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to parse duration %q: %s\n", *onlyRevalOlderThanStr, err)
+			os.Exit(1)
+		}
+	}
+
 	proxy := apiproxy.NewCachingSingleHostReverseProxy(targetURL)
 	cachingTransport := proxy.Transport.(*httpcache.Transport)
 	cachingTransport.Transport = &apiproxy.RevalidationTransport{
 		Check: apiproxy.ValidatorFunc(func(url *url.URL, age time.Duration) bool {
-			return !*neverRevalidate
+			if *neverRevalidate {
+				return true
+			}
+			if *onlyRevalOlderThanStr != "" {
+				return age <= onlyRevalOlderThan
+			}
+			return false
 		}),
 	}
 
 	http.Handle("/", proxy)
 
-	log.Printf("Starting proxy on %s with target %s\n", *bindAddr, targetURL.String())
+	fmt.Fprintf(os.Stderr, "Starting proxy on %s with target %s\n", *bindAddr, targetURL.String())
 	err = http.ListenAndServe(*bindAddr, nil)
 	if err != nil {
 		log.Fatalf("ListenAndServe: %s", err)
